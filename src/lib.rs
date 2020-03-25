ff4a3bb60ad0f0f75f74c951b2189a7d968b1cc3
tanhui <tanhui@corp-ci.com>
2020-03-25 13:35:02 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+
+
+mod wasm {
+    use std::ffi::c_void;
+
+    pub extern "C" fn init (params_ptr: *mut c_void) {
+
+    }
+
+    pub extern "C" fn handle(params_ptr: *mut c_void) {
+        
+    }
+
+    pub extern "C" fn query(params_ptr: *mut c_void) {
+
+    }
+}
