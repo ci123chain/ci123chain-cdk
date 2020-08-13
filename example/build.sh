edf9ca4a298bd1d6e4d020a1997f9ec89a6a6989
Ark <arkrz@outlook.com>
2020-08-13 09:38:54 +0800

    更新 build.sh

--- example/build.sh
+++ example/build.sh
@@ -2,12 +2,18 @@
 
 cargo build --target wasm32-unknown-unknown --release
 
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 cd ./target
 
 cp ./wasm32-unknown-unknown/release/*.wasm ./
 
+# https://github.com/WebAssembly/wabt
 wasm-strip ./*.wasm
 
+# https://github.com/WebAssembly/binaryen
 wasm-opt -Oz ./*.wasm -o example.wasm
 
 cd ..
